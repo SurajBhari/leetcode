b1e23747eb7f3b95886854d060d384c919cd5cb9
Suraj Bhari <45149585+SurajBhari@users.noreply.github.com>
2024-03-12 11:39:54 +0530

    Time: 283 ms (5.08%) | Memory: 16.6 MB (47.71%) - LeetSync

---
+++ 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
+        su = 0 
+        matrix = len(grid)
+        for n in range(len(grid[0])):
+            for m in range(len(grid[n])):
+                n1 = []
+                n2 = []
+                n1.extend([grid[n][i] for i in range(matrix)])
+                n2.extend([grid[i][m] for i in range(matrix)])
+                maximum = min([max(n1), max(n2)])
+                print(maximum, n1,n2)
+                su += maximum - grid[n][m]
+        return su
+                
+        
