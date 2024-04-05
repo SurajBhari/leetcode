7d3108a1996695ef6b92678f3a80d9f2d5f50002
Suraj Bhari <45149585+SurajBhari@users.noreply.github.com>
2024-04-06 00:12:00 +0530

    Time: 46 ms (9.86%) | Memory: 16.6 MB (11.30%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.py
@@ -0,0 +1,20 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        while True:
+            clean_pass = True
+            i = 0
+            while i < len(s):   
+                try:
+                    stmt1 = s[i].upper() == s[i+1]
+                    stmt2 = s[i] == s[i+1].upper()
+                    stmt3 = s[i] == s[i+1]
+                    if ((stmt1) or (stmt2)) and not stmt3:
+                        clean_pass = False
+                        print(s[i], s[i+1], i,i+1, stmt1, stmt2)
+                        s = s.replace(s[i]+s[i+1], "")
+                except IndexError:
+                    break
+                i += 1
+            if clean_pass:
+                break
+        return s
