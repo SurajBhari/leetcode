31d1a79b375d58744f64923e9c3badf20d3697a8
Suraj Bhari <45149585+SurajBhari@users.noreply.github.com>
2024-07-03 01:05:57 +0530

    Time: 48 ms (22.03%) | Memory: 17.7 MB (10.69%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -4,10 +4,13 @@ class Solution:
         for x in range(1,n+1):
             if x%15==0:
                 l.append("FizzBuzz")
+                continue
             elif x%5==0:
                 l.append("Buzz")
+                continue
             elif x%3==0:
                 l.append("Fizz")
+                continue
             else:
                 l.append(str(x))
         return l
